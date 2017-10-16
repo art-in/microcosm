f38efaa5e03f5cfefaf28821a7f58a7c70695a4e
artin-phares <artin.phares@gmail.com>
2017-10-16 16:44:14 +0300

    fix: restore vm state after remap from model

--- src/vm/mutators/default.js
+++ src/vm/mutators/default.js
@@ -6,7 +6,10 @@ import toGraph from 'vm/map/mappers/mindmap-to-graph';
  */
 export default function defaultMutator(state) {
 
-    // by default, re-map from model as simpliest coding
+    // save vm specific part of state
+    const {zoomInProgress} = state.vm.main.mindmap.graph;
+
+    // by default, remap from model as simpliest coding
     // approach (not best performance though).
     // some model changes can radically change viewmodel
     // (eg. moving viewbox can remove bunch of nodes and add
@@ -14,7 +17,9 @@ export default function defaultMutator(state) {
     // almost all nodes because of shading)
     // instead of doing clever patches on existing graph,
     // it is simplier to rebuild whole graph from stratch.
-    // this will clear all vm state specifics, which is not
-    // part of model state (like state of dropdowns, lookups, etc.)
     state.vm.main.mindmap.graph = toGraph(state.model.mindmap);
+
+    // since remapping from model clears vm specific part of state
+    // (like state of dropdowns, lookups, etc), we need to restore it
+    state.vm.main.mindmap.graph.zoomInProgress = zoomInProgress;
 }
