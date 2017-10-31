c0a3d3e5df5db98f26b1fa8655a9e2f110dc4f94
artin-phares <artin.phares@gmail.com>
2017-10-31 23:10:31 +0300

    add BFS alg of graph traversal
    BFS  = breadth-first search

--- src/utils/graph/traverse-graph.js
+++ src/utils/graph/traverse-graph.js
@@ -1,37 +1,84 @@
 /**
- * Generic function for depth-first graph traversal,
- * which calls function on each node of the graph
+ * Generic function for graph traversal
  * 
  * @param {object} node - root node
  * @param {function} visit - function to call on each node
- * @param {bool} [order='pre'] - pre-order or post-order traversal
- * @param {Set} visitedNodes
+ * @param {string} [mode='dfs-pre'] - dfs-pre  - depth-first search pre-order
+ *                                    dfs-post - depth-first search post order
+ *                                    bfs      - breadth-first search
  */
 export default function traverseGraph(
     node,
     visit,
-    order = 'pre',
-    visitedNodes = new Set()) {
+    mode = 'dfs-pre') {
     
+    switch (mode) {
+    case 'dfs-pre':
+    case 'dfs-post':
+        dfs(node, visit, mode);
+        break;
+    case 'bfs':
+        bfs(node, visit);
+        break;
+    default:
+        throw Error(`Unknown traversal mode '${mode}'`);
+    }
+}
+
+/**
+ * Depth-first search (DFS)
+ * 
+ * @param {object} node 
+ * @param {function} visit 
+ * @param {string} mode 
+ * @param {Set} visitedNodes 
+ */
+function dfs(node, visit, mode, visitedNodes = new Set()) {
     if (visitedNodes.has(node)) {
         return;
     }
 
     visitedNodes.add(node);
 
-    if (order === 'pre') {
+    if (mode === 'dfs-pre') {
         visit(node);
     }
 
     node.linksOut.forEach(link => {
-        traverseGraph(
+        dfs(
             link.to,
             visit,
-            order,
+            mode,
             visitedNodes);
     });
     
-    if (order === 'post') {
+    if (mode === 'dfs-post') {
         visit(node);
+    }
+}
+
+/**
+ * Breadth-first search (BFS)
+ * 
+ * @param {object} node 
+ * @param {function} visit 
+ */
+function bfs(node, visit) {
+
+    const visitedNodes = new Set();
+
+    const queue = [];
+    queue.unshift(node);
+
+    while (queue.length) {
+
+        const currentNode = queue.pop();
+
+        visit(currentNode);
+        visitedNodes.add(currentNode);
+
+        currentNode.linksOut
+            .filter(l => !visitedNodes.has(l.to))
+            .forEach(l => queue.unshift(l.to));
     }
 }
--- test/unit/tests/utils/graph/traverse-graph.test.js
+++ test/unit/tests/utils/graph/traverse-graph.test.js
@@ -7,141 +7,244 @@ import traverse from 'src/utils/graph/traverse-graph';
 
 describe('traverse-graph', () => {
 
-    it('should visit each node once', () => {
+    describe('should visit each node once', () => {
 
-        // setup tree graph
-        //
-        //       (root idea)
-        //         /     \
-        //    (idea 1)  (idea 2)
-        //                 \
-        //               (idea 3)
-        //
-        const rootIdea = new Idea({id: 'root idea', isRoot: true});
-        const idea1 = new Idea({id: 'idea 1'});
-        const idea2 = new Idea({id: 'idea 2'});
-        const idea3 = new Idea({id: 'idea 3'});
+        // eslint-disable-next-line require-jsdoc
+        function setupGraph() {
+            // setup tree graph
+            //
+            //       (root idea)
+            //         /     \
+            //    (idea 1)  (idea 3)
+            //       /         \
+            //   (idea 2)     (idea 4)
+            //
+            const rootIdea = new Idea({id: 'root idea', isRoot: true});
+            const idea1 = new Idea({id: 'idea 1'});
+            const idea2 = new Idea({id: 'idea 2'});
+            const idea3 = new Idea({id: 'idea 3'});
+            const idea4 = new Idea({id: 'idea 4'});
 
-        const assoc1 = new Association({
-            id: 'assoc 1',
-            from: rootIdea,
-            to: idea1
+            const assocRootTo1 = new Association({
+                from: rootIdea,
+                to: idea1
+            });
+
+            const assocRootTo3 = new Association({
+                from: rootIdea,
+                to: idea3
+            });
+
+            const assoc1to2 = new Association({
+                from: idea1,
+                to: idea2
+            });
+
+            const assoc3to4 = new Association({
+                from: idea3,
+                to: idea4
+            });
+
+            rootIdea.associationsOut = [assocRootTo1, assocRootTo3];
+            idea1.associationsIn = [assocRootTo1];
+            idea1.associationsOut = [assoc1to2];
+            idea2.associationsIn = [assoc1to2];
+            idea3.associationsIn = [assocRootTo3];
+            idea3.associationsOut = [assoc3to4];
+            idea4.associationsIn = [assoc3to4];
+
+            return rootIdea;
+        }
+
+        it('depth-first pre-order', () => {
+
+            // setup
+            const rootIdea = setupGraph();
+            const visited = [];
+
+            // target
+            traverse(
+                rootIdea,
+                idea => visited.push(idea.id),
+                'dfs-pre');
+
+            // check
+            expect(visited).to.deep.equal([
+                'root idea',
+                'idea 1',
+                'idea 2',
+                'idea 3',
+                'idea 4'
+            ]);
         });
 
-        const assoc2 = new Association({
-            id: 'assoc 2',
-            from: rootIdea,
-            to: idea2
+        it('depth-first post-order', () => {
+
+            // setup
+            const rootIdea = setupGraph();
+            const visited = [];
+
+            // target
+            traverse(
+                rootIdea,
+                idea => visited.push(idea.id),
+                'dfs-post');
+
+            // check
+            expect(visited).to.deep.equal([
+                'idea 2',
+                'idea 1',
+                'idea 4',
+                'idea 3',
+                'root idea'
+            ]);
         });
-
-        rootIdea.associationsOut = [
-            assoc1,
-            assoc2
-        ];
-
-        const assoc3 = new Association({
-            id: 'assoc 3',
-            from: idea2,
-            to: idea3
-        });
-
-        idea1.associationsOut = [];
-        idea3.associationsOut = [];
-        idea2.associationsOut = [
-            assoc3
-        ];
-
-        const visited = {};
-
-        // target
-        traverse(rootIdea, idea => {
-            visited[idea.id] = (visited[idea.id] || 0) + 1;
-        });
-
-        // check
-        expect(visited).to.deep.equal({
-            ['root idea']: 1,
-            ['idea 1']: 1,
-            ['idea 2']: 1,
-            ['idea 3']: 1
-        });
-    });
-
-    it('should traverse graph with cycles', () => {
         
-        // setup tree graph
-        //
-        //       (root idea)
-        //         /     \
-        //    (idea 1)  (idea 2) <------
-        //                 \            \
-        //               (idea 3) --> (idea 4)
-        //
-        const rootIdea = new Idea({id: 'root idea', isRoot: true});
-        const idea1 = new Idea({id: 'idea 1'});
-        const idea2 = new Idea({id: 'idea 2'});
-        const idea3 = new Idea({id: 'idea 3'});
-        const idea4 = new Idea({id: 'idea 4'});
+        it('breadth-first', () => {
 
-        const assoc1 = new Association({
-            id: 'assoc 1',
-            from: rootIdea,
-            to: idea1
+            // setup
+            const rootIdea = setupGraph();
+            const visited = [];
+
+            // target
+            traverse(
+                rootIdea,
+                idea => visited.push(idea.id),
+                'bfs');
+
+            // check
+            expect(visited).to.deep.equal([
+                'root idea',
+                'idea 1',
+                'idea 3',
+                'idea 2',
+                'idea 4'
+            ]);
         });
 
-        const assoc2 = new Association({
-            id: 'assoc 2',
-            from: rootIdea,
-            to: idea2
+    });
+    
+    describe('should traverse graph with cycles', () => {
+
+        // eslint-disable-next-line require-jsdoc
+        function setupGraph() {
+            // setup tree graph
+            //
+            //       (root idea)
+            //         /     \
+            //    (idea 1)  (idea 2) <------
+            //                 \            \
+            //               (idea 3) --> (idea 4)
+            //
+            const rootIdea = new Idea({id: 'root idea', isRoot: true});
+            const idea1 = new Idea({id: 'idea 1'});
+            const idea2 = new Idea({id: 'idea 2'});
+            const idea3 = new Idea({id: 'idea 3'});
+            const idea4 = new Idea({id: 'idea 4'});
+
+            const assocRootTo1 = new Association({
+                from: rootIdea,
+                to: idea1
+            });
+
+            const assocRootTo2 = new Association({
+                from: rootIdea,
+                to: idea2
+            });
+
+            const assoc2to3 = new Association({
+                from: idea2,
+                to: idea3
+            });
+
+            const assoc3to4 = new Association({
+                from: idea3,
+                to: idea4
+            });
+
+            const assoc4to2 = new Association({
+                from: idea4,
+                to: idea2
+            });
+
+            rootIdea.associationsOut = [assocRootTo1, assocRootTo2];
+            idea1.associationsIn = [assocRootTo1];
+            idea2.associationsIn = [assocRootTo2, assoc4to2];
+            idea2.associationsOut = [assoc2to3];
+            idea3.associationsIn = [assoc2to3];
+            idea3.associationsOut = [assoc3to4];
+            idea4.associationsIn = [assoc3to4];
+            idea4.associationsOut = [assoc4to2];
+
+            return rootIdea;
+        }
+
+        it('depth-first pre-order', () => {
+            
+            // setup
+            const rootIdea = setupGraph();
+            const visited = [];
+
+            // target
+            traverse(
+                rootIdea,
+                idea => visited.push(idea.id),
+                'dfs-pre');
+
+            // check
+            expect(visited).to.deep.equal([
+                'root idea',
+                'idea 1',
+                'idea 2',
+                'idea 3',
+                'idea 4'
+            ]);
         });
 
-        rootIdea.associationsOut = [
-            assoc1,
-            assoc2
-        ];
+        it('depth-first post-order', () => {
+            
+            // setup
+            const rootIdea = setupGraph();
+            const visited = [];
 
-        const assoc3 = new Association({
-            id: 'assoc 3',
-            from: idea2,
-            to: idea3
+            // target
+            traverse(
+                rootIdea,
+                idea => visited.push(idea.id),
+                'dfs-post');
+
+            // check
+            expect(visited).to.deep.equal([
+                'idea 1',
+                'idea 4',
+                'idea 3',
+                'idea 2',
+                'root idea'
+            ]);
         });
 
-        idea1.associationsOut = [];
-        idea2.associationsOut = [
-            assoc3
-        ];
+        it('breadth-first', () => {
+            
+            // setup
+            const rootIdea = setupGraph();
+            const visited = [];
 
-        const assoc4 = new Association({
-            id: 'assoc 4',
-            from: idea4,
-            to: idea4
+            // target
+            traverse(
+                rootIdea,
+                idea => visited.push(idea.id),
+                'bfs');
+
+            // check
+            expect(visited).to.deep.equal([
+                'root idea',
+                'idea 1',
+                'idea 2',
+                'idea 3',
+                'idea 4'
+            ]);
         });
 
-        idea3.associationsOut = [assoc4];
-
-        const assoc5 = new Association({
-            id: 'assoc 5',
-            from: idea4,
-            to: idea2
-        });
-
-        idea4.associationsOut = [assoc5];
-
-        const visited = {};
-
-        // target
-        traverse(rootIdea, idea => {
-            visited[idea.id] = (visited[idea.id] || 0) + 1;
-        });
-
-        // check
-        expect(visited).to.deep.equal({
-            ['root idea']: 1,
-            ['idea 1']: 1,
-            ['idea 2']: 1,
-            ['idea 3']: 1,
-            ['idea 4']: 1
-        });
     });
 
 });
