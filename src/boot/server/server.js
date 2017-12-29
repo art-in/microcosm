f96752b9f6ec527aa7b7c83d875dc4c65ea81641
artin-phares <artin.phares@gmail.com>
2017-12-29 21:05:45 +0300

    #70 gzip server responses

--- src/boot/server/server.js
+++ src/boot/server/server.js
@@ -3,8 +3,21 @@ import config from '../../config';
 
 import express from 'express';
 import logger from 'morgan';
+import compression from 'compression';
 
 const app = express();
+
+// compress all responses
+// Q: why compress on the fly and not pre-compress when building?
+// A: it is not worth the trouble since we will use aggressive client side
+//    caching (service worker cache). and the trouble is that some files do not
+//    need additional compression (eg. woff fonts) which is determined by
+//    compressor dynamically, but urls are not updated dynamically
+//    (ie. url-loader does not respect .gz extension added to compressed files
+//     by compression-webpack-plugin, which result in 404s. and if do not modify
+//     filename we will not know when to add 'Content-Encoding' response header)
+app.use(compression());
+
 app.use(logger('dev'));
 
 app.use(express.static(config.server.static.folder));
