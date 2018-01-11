0d0953c7a938e82d5fbc95fc42fcee12d88e2947
artin-phares <artin.phares@gmail.com>
2018-01-11 23:03:56 +0300

    fix do not add additional new lines to node pipe data

--- tasks/serve/static/index.js
+++ tasks/serve/static/index.js
@@ -1,10 +1,12 @@
 const config = require('../../../config');
 const spawn = require('child_process').spawn;
 
+const {stdout, stderr} = process;
+
 module.exports = () => {
     const ps = spawn('node', [config.src.serv.output.entry]);
 
-    ps.stdout.on('data', data => console.log(data.toString()));
-    ps.stderr.on('data', data => console.log(data.toString()));
-    ps.on('exit', code => console.log(`node exited (code ${code.toString()})`));
+    ps.stdout.on('data', data => stdout.write(data.toString()));
+    ps.stderr.on('data', data => stderr.write(data.toString()));
+    ps.on('exit', code => stdout.write(`node exited (${code.toString()})`));
 };
