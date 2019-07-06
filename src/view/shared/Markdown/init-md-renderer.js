cd672c48079ef99f6ee54b4f3ca4409fe7b42dca
art-in <artin@artin.space>
2019-07-06 11:49:47 +0000

    #100 add target="_blank" attribute to all links

--- src/view/shared/Markdown/init-md-renderer.js
+++ src/view/shared/Markdown/init-md-renderer.js
@@ -38,4 +38,24 @@ md.use(TaskListsPlugin, {
   label: false
 });
 
+// add target="_blank" attribute to all links
+// https://github.com/markdown-it/markdown-it/blob/master/docs/architecture.md#renderer
+var defaultLinkRenderer =
+  md.renderer.rules.link_open ||
+  function(tokens, idx, options, env, self) {
+    return self.renderToken(tokens, idx, options);
+  };
+
+md.renderer.rules.link_open = function(tokens, idx, options, env, self) {
+  var aIndex = tokens[idx].attrIndex('target');
+
+  if (aIndex < 0) {
+    tokens[idx].attrPush(['target', '_blank']);
+  } else {
+    tokens[idx].attrs[aIndex][1] = '_blank';
+  }
+
+  return defaultLinkRenderer(tokens, idx, options, env, self);
+};
+
 export default md;
